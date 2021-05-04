1a8731c8a3fce295bdd8598940517652d2e51c00
TranThiDieuHien <72079838+TranThiDieuHien@users.noreply.github.com>
2021-05-04 08:32:08 +0700

    Add files via upload

---
+++ tinh_giai_thua.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  4 08:26:45 2021
+
+@author: Tran Thi Dieu Hien
+"""
+
+#Tính giai thừa thông thường
+def tinhgiaithua(n):
+    giai_thua = 1;
+    if (n == 0 or n == 1):
+        return giai_thua
+    else:
+        for i in range(2, n + 1):
+            giai_thua = giai_thua * i
+        return giai_thua
+ 
+n = int(input("Nhập số nguyên dương n = "))
+print("Giai thừa của", n, "là", tinhgiaithua(n))
+
+#Tính giai thừa theo đệ quy
+n = int(input("Nhập số cần tính giai thừa: "))
+ 
+def giaiThua(n):
+    if n == 0:
+        return 1
+    return n * giaiThua(n - 1)
+ 
+print (giaiThua(n))
